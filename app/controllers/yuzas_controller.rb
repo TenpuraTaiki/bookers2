a7a6726d63e901289b207e814568fd6264c1f532
TenpuraTaiki <sleeping.3.fairies@gmail.com>
2018-05-28 06:55:03 +0900

    再提出

--- app/controllers/yuzas_controller.rb
+++ app/controllers/yuzas_controller.rb
@@ -7,12 +7,18 @@ class YuzasController < ApplicationController
 
   def edit
     @yuza = Yuza.find(params[:id])
+      if @yuza != current_yuza
+        redirect_to hons_path, notice:'＜error＞自分以外のマイページは編集することができません'      
+      end
   end
 
   def update
     @yuza = Yuza.find(params[:id])
-    @yuza.update(yuza_params)
-    redirect_to yuza_path(@yuza.id)
+    if @yuza.update(yuza_params)
+      redirect_to yuza_path(@yuza.id)
+    else
+      redirect_to yuza_path(@yuza.id), notice: '自己紹介は５０文字以内にしてください'
+    end
   end
 
   private
--- app/models/yuza.rb
+++ app/models/yuza.rb
@@ -7,6 +7,7 @@ class Yuza < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { in: 2..20 }
+  validates :yuza_jikosyo, length: { maximum: 50 }
 
   has_many :hons
 
